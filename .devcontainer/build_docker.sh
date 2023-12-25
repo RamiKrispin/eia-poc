1ea7657db0653346b735c08214d673f57115d744
RamiKrispin <ramkrisp@umich.edu>
2023-12-25 00:38:00 -0800

    update the docker image

---
+++ .devcontainer/build_docker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Build the docker"
+
+docker build . -f Dockerfile \
+               --progress=plain \
+               --build-arg QUARTO_VER="1.3.450" \
+               --build-arg VENV_NAME="forecasting-poc" \
+               -t rkrispin/forecast-poc:0.0.0.9000
+
+if [[ $? = 0 ]] ; then
+echo "Pushing docker..."
+docker push rkrispin/forecast-poc:0.0.0.9000
+else
+echo "Docker build failed"
+fi
--- .devcontainer/install_dependencies.sh
+++ .devcontainer/install_dependencies.sh
@@ -1,4 +1,4 @@
-
+#!/usr/bin/env bash
 
 apt-get update && apt-get install -y --no-install-recommends \
     sudo \
