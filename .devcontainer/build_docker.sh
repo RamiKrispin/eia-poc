d6955acfc06c193fa391e9b3fd12ba2b29584543
RamiKrispin <ramkrisp@umich.edu>
2023-12-31 02:10:22 -0800

    merge with origin

--- .devcontainer/build_docker.sh
+++ .devcontainer/build_docker.sh
@@ -2,15 +2,16 @@
 
 echo "Build the docker"
 
-docker build . -f Dockerfile \
+docker buildx build . -f Dockerfile \
+               --platform linux/amd64, linux/arm64 \
                --progress=plain \
                --build-arg QUARTO_VER="1.3.450" \
                --build-arg VENV_NAME="forecasting-poc" \
-               -t rkrispin/forecast-poc:0.0.0.9001
+               -t rkrispin/forecast-poc:0.0.0.9002
 
 if [[ $? = 0 ]] ; then
 echo "Pushing docker..."
-docker push rkrispin/forecast-poc:0.0.0.9001
+docker push rkrispin/forecast-poc:0.0.0.9002
 else
 echo "Docker build failed"
 fi
