16ab2e5de988dcbf7ec5b0ddd9d75a5c761b51d7
RamiKrispin <ramkrisp@umich.edu>
2024-01-19 22:34:49 -0800

    adding new dependencies - darts and data profile libraries

--- .devcontainer/build_docker.sh
+++ .devcontainer/build_docker.sh
@@ -6,11 +6,11 @@ docker build . -f Dockerfile.multiple \
                --progress=plain \
                --build-arg QUARTO_VER=$QUARTO_VER \
                --build-arg VENV_NAME="forecasting-poc" \
-               -t rkrispin/forecast-poc:0.0.0.9007
+               -t rkrispin/forecast-poc:0.0.0.9008
 
 if [[ $? = 0 ]] ; then
 echo "Pushing docker..."
-docker push rkrispin/forecast-poc:0.0.0.9007
+docker push rkrispin/forecast-poc:0.0.0.9008
 else
 echo "Docker build failed"
 fi
